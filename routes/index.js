6bdef35f870407383885ccbdf2e493a4f9e35acb
Rahman Younus <rahman@industrious.agency>
2019-01-04 16:20:33 +0000

    added middleware folder and index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+const middlewares = require('../middleware')
 const controllers = require("../controllers");
 
 // Do work here
