c62fe16143a8b77f56e54a092cb6fb186486cce0
Rahman Younus <rahman@industrious.agency>
2019-01-22 15:33:51 +0000

    get stores with tags

--- controllers/tag.js
+++ controllers/tag.js
@@ -4,8 +4,11 @@ const Store = mongoose.model("Store");
 module.exports = tag = {
   get: async (req, res) => {
     const currentTag = req.params.tag;
-    const tags = await Store.getTagList();
+    const getTags = Store.getTagList();
+    //return current tag or all stores with tags if not specified
+    const getStores = Store.find({ tags: currentTag || {$exists: true} });
+    const [tags, stores] = await Promise.all([getTags, getStores]);
 
-    res.render("tags/index", { title: "Tags", tags, currentTag });
+    res.render("tags/index", { title: "Tags", tags, currentTag, stores });
   }
 }
